57f47df86e11e55e7c60dbbb3eb8f02799799896
Leonardo Astros <leonardo@vauxoo.com>
2023-12-01 13:10:24 -0400

    [REF] Refactored to use github.com/jackc/pgx. This method works well also with usual github.com/lib/pq

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
     "context"
+    "database/sql"
     "log"
     "testEntGo/ent"
-    // _ "github.com/jackc/pgx/v4/stdlib"
-    _ "github.com/lib/pq"
+
+    "entgo.io/ent/dialect"
+    entsql "entgo.io/ent/dialect/sql"
+    _ "github.com/jackc/pgx/v5/stdlib"
+    // _ "github.com/lib/pq"
 )
 
 var (
-	pgDriver = "postgres" // "pgx" //This is the name of the postgres driver registered by jackc/pgx
+	pgDriver = "pgx" //This is the name of the postgres driver registered by jackc/pgx
 )
 
+func getClient(dbURL string) (*ent.Client, error) {
+    // Open Database
+    db, err := sql.Open(pgDriver, dbURL)
+    if err != nil {
+        return nil, err
+    }
+
+    // Create driver and return
+    driver := entsql.OpenDB(dialect.Postgres, db)
+    return ent.NewClient(ent.Driver(driver)), nil
+}
+
 func main() {
-    client, err := ent.Open(pgDriver, "host=localhost port=5432 user=testuser dbname=test_entgo password=testpswd")
+    // client, err := ent.Open(pgDriver, "host=localhost port=5432 user=testuser dbname=test_entgo password=testpswd")
+    client, err := getClient("host=localhost port=5432 user=testuser dbname=test_entgo password=testpswd")
     if err != nil {
         log.Fatalf("failed opening connection to postgres: %v", err)
     }
