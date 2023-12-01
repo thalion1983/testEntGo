3f42008c2592f63ee40a48d3757c48154763f250
Leonardo Astros <leonardo@vauxoo.com>
2023-12-01 11:23:40 -0400

    [ADD] First schema

---
+++ ent/schema/people.go
@@ -0,0 +1,25 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// People holds the schema definition for the People entity.
+type People struct {
+	ent.Schema
+}
+
+// Fields of the People.
+func (People) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name"),
+		field.String("last_name"),
+		field.Int("age").Positive(),
+	}
+}
+
+// Edges of the People.
+func (People) Edges() []ent.Edge {
+	return nil
+}
