0750a28528d46857d528546acc62142dc9181430
Leonardo Astros <leonardo@vauxoo.com>
2023-12-01 13:40:53 -0400

    [ADD] Added new record creation in entity People

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "context"
-    "database/sql"
-    "log"
-    "testEntGo/ent"
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"testEntGo/ent"
 
-    "entgo.io/ent/dialect"
-    entsql "entgo.io/ent/dialect/sql"
-    _ "github.com/jackc/pgx/v5/stdlib"
-    // _ "github.com/lib/pq"
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	// _ "github.com/lib/pq"
 )
 
 var (
@@ -28,6 +29,20 @@ func getClient(dbURL string) (*ent.Client, error) {
     return ent.NewClient(ent.Driver(driver)), nil
 }
 
+// creates a test record of people
+func createPeople(ctx context.Context, cli *ent.Client) (*ent.People, error) {
+    res, err := cli.People.Create().
+        SetName("Primera").
+        SetLastName("Persona").
+        SetAge(23).
+        Save(ctx)
+
+    if err != nil {
+        return nil, fmt.Errorf("while creating new people: %w", err)
+    }
+    return res, nil
+}
+
 func main() {
     // client, err := ent.Open(pgDriver, "host=localhost port=5432 user=testuser dbname=test_entgo password=testpswd")
     client, err := getClient("host=localhost port=5432 user=testuser dbname=test_entgo password=testpswd")
@@ -38,5 +53,12 @@ func main() {
     // Run the auto migration tool.
     if err := client.Schema.Create(context.Background()); err != nil {
         log.Fatalf("failed creating schema resources: %v", err)
+    }
+
+    ctx := context.Background() // context in use
+
+    // Create a record on people
+    if _, err := createPeople(ctx, client); err != nil {
+        log.Fatalf("failed creating new people: %v", err)
     }
 }
